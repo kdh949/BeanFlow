78d46fdc4cb70ddb42681b7dded0a9aa920d9736
Donghyun Kim <kdh949@naver.com>
2026-08-24 00:39:39 +0900

    fix: 지원 본인확인 Story 검증 대기 보강

--- frontend/src/features/support/SupportWorkspacePage.stories.tsx
+++ frontend/src/features/support/SupportWorkspacePage.stories.tsx
@@ -145,7 +145,7 @@ export const VerifiedGrantReveal: Story = {
     await openCase(canvas);
     await userEvent.click(canvas.getByRole("button", { name: "ENHANCED 본인확인 시작" }));
     await userEvent.click(await canvas.findByRole("button", { name: "등록 전화로 challenge 발급" }));
-    await userEvent.type(canvas.getByLabelText("일회성 proof"), "123456");
+    await userEvent.type(await canvas.findByLabelText("일회성 proof"), "123456");
     await userEvent.click(canvas.getByRole("button", { name: "proof 검증" }));
     await userEvent.click(await canvas.findByRole("button", { name: "전화번호 Grant 요청" }));
     await userEvent.click(await canvas.findByRole("button", { name: "승인된 전화번호 열람" }));
